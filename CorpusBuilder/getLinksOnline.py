f4652e4e69857d1a187c24a709d381c6749376a8
Daria <dasham94@yandex.ru>
2016-02-07 01:23:24 +0300

    многопоточность при скачивании ссылок online

--- CorpusBuilder/getLinksOnline.py
+++ CorpusBuilder/getLinksOnline.py
@@ -10,17 +10,18 @@ trash = [u'File:', u'Portal:', u'Main_Page', u'Wikipedia:', u'Help:', u'Special:
          u'Template_talk:']
 
 
-
+# take links with tag "href"
 def is_wiki_links(tag):
-    if tag.has_attr('href') and tag['href'][:6]=='/wiki/':
+    if tag.has_attr('href') and tag['href'][:6] == '/wiki/':
         for name in trash:
-            if tag['href'].find(name)!=-1:
+            if tag['href'].find(name) != -1:
                 return False
         return True
     else:
         return False
 
 
+# estimate links: takes only one links for mention
 def mention_estimate(mention, mentions):
     best_href = ''
     if mention != '':
@@ -36,19 +37,21 @@ def mention_estimate(mention, mentions):
 
 import re
 
+# get links using url of article
 def get_links_online(url):
     url = url.strip()
     text = requests.get(url).text
-    print url
     soup = BeautifulSoup(text)
     title = soup.title.text.encode('utf-8')
     dirName = "pioNER_Wiki_Links/different"
     if re.match(r'[A-Z]', url[len("https://en.wikipedia.org/wiki/"):]) is not None:
         dirName = "pioNER_Wiki_Links/" + url[len("https://en.wikipedia.org/wiki/")]
+    else:
+        return
 
     links = open(dirName + '/'+url[len("https://en.wikipedia.org/wiki/"):].replace('_', ' '), 'w')
     links.write(url[len("https://en.wikipedia.org/")-1:]+'\t'
-                +title[:-len(' - Wikipedia, the free encyclopedia')] +'\n')
+                +title[:-len(' - Wikipedia, the free encyclopedia')] + '\n')
     hrefs = defaultdict(set)
     mentions = defaultdict(set)
     wiki_links = soup.find_all(is_wiki_links)
@@ -61,8 +64,10 @@ def get_links_online(url):
     for mention in mentions:
         #links.write(href)
         #for mention in hrefs[href]:
-        #if Levenshtein.distance(mention_estimate(mention)[6:], mention)*2./(1+len(mention_estimate(mention)[6:] + mention)) < 0.8:
-        if Levenshtein.setratio(mention_estimate(mention, mentions)[6:].split('_'), mention.replace('\xc2\xa0', ' ').split()) >= 0.5:
+        #if Levenshtein.distance(mention_estimate(mention)[6:], mention)
+        # *2./(1+len(mention_estimate(mention)[6:] + mention)) < 0.8:
+        if Levenshtein.setratio(mention_estimate(mention, mentions)[6:].split('_'),
+                                mention.replace('\xc2\xa0', ' ').split()) >= 0.5:
             #print mention_estimate(mention, mentions), mention
             links.write(mention_estimate(mention, mentions) + '\t'+mention)
             links.write('\n')
@@ -89,9 +94,10 @@ def get_links_online_for_corpus(processes=multiprocessing.cpu_count()):
         if listdir.count(dirName) == 0:
             os.mkdir("pioNER_Wiki_Links/" + dirName)
 
-        for dirName in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']:
+        for dirName in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+                        'T', 'U', 'V', 'W', 'X', 'Y', 'Z']:
             if listdir.count(dirName) == 0:
-                os.mkdir("pioNER_Wiki_Links/" + dirName)
+               os.mkdir("pioNER_Wiki_Links/" + dirName)
 
 
 
@@ -104,8 +110,7 @@ def get_links_online_for_corpus(processes=multiprocessing.cpu_count()):
             corpus_contents_file.close()
         pool = multiprocessing.Pool(processes)
         corpus_contents_file = open("contents", 'r')
-        for url_line in corpus_contents_file:
-            get_links_online(url_line)
+        pool.map(get_links_online, corpus_contents_file)
         pool.terminate()
 
 
